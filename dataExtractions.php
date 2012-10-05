a57d370044905e7883cc93337fbd425ae212e004
mtasuandi <teguh.andro@gmail.com>
2012-10-05 20:46:16 +0700

    Update dataExtractions.php
    Modify CitySearch, InsiderPages. Add Yelp and Google Places. 

--- dataExtractions.php
+++ dataExtractions.php
@@ -67,26 +67,47 @@ switch($_GET['Action']){
 	
 		# URL example http://denver.citysearch.com/profile/1782729/arvada_co/namiko_s.html
 		$uri = $_GET['url'];
-		$html = file_get_html($uri);
-
+		$content = file_get_contents($uri);
+		$content = str_replace("review.description.","reviewdescription",$content);
+		
+		$html = new simple_html_dom();
+		$html->load($content);
+		$url = $uri;
+		
 		foreach($html->find('strong.votes') as $element){
-			$number_of_ratings = $element;
+		    $number_of_ratings = $element;
 			$num = strip_tags($number_of_ratings);
+			
 		}
 		foreach($html->find('span.average') as $element){ 
-			$ave =  strip_tags($element);
-			$aveee = $ave;
+		    $ave =  strip_tags($element);
+			$average = $ave;
+			$av = $num * ($average/100);
 		}
 		foreach($html->find('span#csReviewCount') as $element){
-			$num_rev = strip_tags($element);
+		    $num_rev = strip_tags($element);
 			$numb = $num_rev;
 		}
-		foreach($html->find('p.description') as $element){
-			$r  = $element->plaintext."|"."<br/>";
-			$ar = array("'");
-			$r2 = str_replace($ar, "", $r);
-			
-		}	
+		foreach($html->find('p#reviewdescription1') as $element){
+			$review1  = $element->plaintext;
+			$rev1 = str_replace("'","",$review1);
+		}
+		foreach($html->find('p#reviewdescription2') as $element){
+			$review2  = $element->plaintext;
+			$rev2 = str_replace("'","",$review2);
+		}
+		foreach($html->find('p#reviewdescription3') as $element){
+			$review3  = $element->plaintext;
+			$rev3 = str_replace("'","",$review3);
+		}
+		foreach($html->find('p#reviewdescription4') as $element){
+			$review4  = $element->plaintext;
+			$rev4 = str_replace("'","",$review4);
+		}
+		foreach($html->find('p#reviewdescription5') as $element){
+			$review5  = $element->plaintext;
+			$rev5 = str_replace("'","",$review5);
+		}
 	break;
 	
 	case 'InsiderPages':
@@ -94,19 +115,104 @@ switch($_GET['Action']){
 		# URL example http://www.insiderpages.com/b/15240030746/athar-restaurant-elmhurst
 		$uri = $_GET['url'];
 		$html = file_get_html($uri);
-
+		$url = $uri;
+		
 		foreach($html->find('span.count') as $element){
-			$number_of_reviews = $element;
-			echo $number_of_reviews;
+		    $number_of_reviews = $element->plaintext;
 		}
-		foreach($html->find('abbr.average') as $element){
-			$average_ratings = $element;
-			echo strlen($average_ratings);
+		foreach($html->find('div.rating_box') as $element){
+		    $average_ratings = GetBetween($element, 'title="', '">');
 		}
+		$reviews = array();
 		foreach($html->find('p.description') as $element){
-			$reviews = $element;
-			echo $reviews;
+		    $reviews[] = $element->plaintext;
 		}
+		
+		$rev1 = "";
+		if(count($reviews) >0) $rev1 = str_replace("'","",$reviews[0]);
+		
+		$rev2 = "";
+		if(count($reviews) >1) $rev2 = str_replace("'","",$reviews[1]);
+		
+		$rev3 = "";
+		if(count($reviews) >2) $rev3 = str_replace("'","",$reviews[2]);
+		
+		$rev4 = "";
+		if(count($reviews) >3) $rev4 = str_replace("'","",$reviews[3]);
+		
+		$rev5 = "";
+		if(count($reviews) >4) $rev5 = str_replace("'","",$reviews[4]);
+
+	break;
+	
+	case '':
+		$uri = $_GET['url'];
+		$url = $uri;
+		$html = file_get_html($uri);
+		
+		foreach($html->find('span.review-count') as $element){
+		    $number_of_reviews = $element;
+			$num = preg_replace("/[^0-9]/", '', $number_of_reviews);
+			
+		}
+		foreach($html->find('div#bizRating') as $element){ 
+		    $ave =  GetBetween($element,'<meta itemprop="ratingValue" content="','.0">');
+		}
+		
+		foreach($html->find('p.review_comment') as $element){
+			$reviews[] = $element->plaintext;
+		}
+		
+		$rev1 = "";
+		if(count($reviews) >0) $rev1 = str_replace("'","",$reviews[0]);
+		
+		$rev2 = "";
+		if(count($reviews) >1) $rev2 = str_replace("'","",$reviews[1]);
+		
+		$rev3 = "";
+		if(count($reviews) >2) $rev3 = str_replace("'","",$reviews[2]);
+		
+		$rev4 = "";
+		if(count($reviews) >3) $rev4 = str_replace("'","",$reviews[3]);
+		
+		$rev5 = "";
+		if(count($reviews) >4) $rev5 = str_replace("'","",$reviews[4]);	
+	break;
+	
+	case 'GooglePlaces':
+		$uri = $_GET['url'];
+		$url = $uri;
+		$html = file_get_html($uri);
+		
+		foreach($html->find('span.qja') as $element){
+		    $number_of_reviews = $element;
+			$num = preg_replace("/[^0-9]/", '', $number_of_reviews);	
+		}
+		
+		foreach($html->find('span.ix') as $element){ 
+			$num_rate[] = $element; //GetBetween($element,'<span class="Vy">OVERALL','</span>');
+		}
+		$num_r = "";
+		if(count($num_rate) > 0) $num_r = $num_rate[0];
+		
+		foreach($html->find('span.aza') as $element){
+			$reviews[] = $element->plaintext;
+		}
+		
+		$rev1 = "";
+		if(count($reviews) >0) $rev1 = str_replace("'","",$reviews[0]);
+		
+		$rev2 = "";
+		if(count($reviews) >1) $rev2 = str_replace("'","",$reviews[1]);
+		
+		$rev3 = "";
+		if(count($reviews) >2) $rev3 = str_replace("'","",$reviews[2]);
+		
+		$rev4 = "";
+		if(count($reviews) >3) $rev4 = str_replace("'","",$reviews[3]);
+		
+		$rev5 = "";
+		if(count($reviews) >4) $rev5 = str_replace("'","",$reviews[4]);
 	break;
 }
 function GetBetween($content,$start,$end){
